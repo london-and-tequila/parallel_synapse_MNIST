8b50e25d12eef42890712db974e9e692df55e008
london-and-tequila <yus027@ucsd.edu>
2023-10-29 13:15:40 -0700

    flip sign parallel layer, ready to use

--- network.py
+++ network.py
@@ -12,44 +12,40 @@ naming convention:
     n_synapse: number of synapse per input
     input_dim: dimension of input
 '''
-
-
-class ParallelSynapse(nn.Module):
+class TwoLayerNN(nn.Module):
     '''
-    Parallel Synapse Module create parallel synapses per input with a given number of synapse
+    two layered neural network
     '''
-
-    def __init__(self, input_dim: int, n_synapse: int, input_range: Tuple = (-1, 1)) -> None:
+    def __init__(self, 
+                input_dim: int = 28*28, 
+                hidden_dim: int = 10, 
+                output_dim: int = 10,
+                drop_out: float = 0.1) -> None:
         super().__init__()
 
-        self.input_dim = input_dim
-        self.n_synapse = n_synapse
-
-        self.thres = nn.Parameter(torch.rand(
-            n_synapse, input_dim) * (input_range[1] - input_range[0]) + input_range[0])
-        self.slope = nn.Parameter(10 * torch.rand(n_synapse, input_dim))
-        self.ampli = nn.Parameter(torch.rand(n_synapse, input_dim))
-
-    def forward(self, input: Tensor):
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(drop_out) 
+    def forward(self, x: Tensor):
         '''
         Inputs:
             input: (n_data, input_dim)
 
         Returns:
-            output: (n_data, input_dim)
+            output: (n_data, output_dim)
         '''
-
-        n_data = input.shape[0]
-        x = self.slope[None, :, :].expand(n_data, self.n_synapse, self.input_dim) * \
-            input[:, None, :].expand(n_data, self.n_synapse, self.input_dim)
-        x = x - self.thres[None, :,:].expand(n_data, self.n_synapse, self.input_dim)
-        x = self.ampli[None, :, :].expand(n_data, self.n_synapse, self.input_dim) * \
-            torch.tanh(x)
-        x = x.sum(dim=1).squeeze(1)
+        x = self.dropout(x)
+        x = self.fc1(x)
+        x = torch.relu(x)
+        x = self.fc2(x)
         return x
 
 class ParallelSynapseLayer(nn.Module):
-    def __init__(self, input_dim: int, n_synapse: int, output_dim: int, input_range: Tuple = (-1, 1)) -> None:
+    def __init__(self, 
+                input_dim: int, 
+                n_synapse: int, 
+                output_dim: int, 
+                input_range: Tuple = (-1, 1)) -> None:
         super().__init__()
         self.input_dim = input_dim
         self.n_synapse = n_synapse
@@ -57,8 +53,12 @@ class ParallelSynapseLayer(nn.Module):
         
         self.thres = nn.Parameter(torch.rand(
             self.n_synapse, self.input_dim, self.output_dim) * (input_range[1] - input_range[0]) + input_range[0])
-        self.slope = nn.Parameter(10 * torch.rand(self.n_synapse, self.input_dim, self.output_dim))
+        # normal distribution for threshold:
+        # self.thres = torch.abs(torch.normal(0, input_range[1] , size=(self.n_synapse, self.input_dim, self.output_dim)) )
+        self.slope = nn.Parameter(5*torch.rand(self.n_synapse, self.input_dim, self.output_dim))
         self.ampli = nn.Parameter(torch.rand(self.n_synapse, self.input_dim, self.output_dim))
+        self.scaler = nn.Parameter(torch.rand(self.input_dim, self.output_dim))
+        
     def forward(self, input: Tensor):
         '''
         Inputs:
@@ -69,176 +69,39 @@ class ParallelSynapseLayer(nn.Module):
         '''
         n_data = input.shape[0]
         x = self.slope[None, :, :,:].expand(n_data, self.n_synapse, self.input_dim, self.output_dim) * \
-            input[:, None, :, None].expand(n_data, self.n_synapse, self.input_dim, self.output_dim)
-        x = x - self.thres[None, :,:,:].expand(n_data, self.n_synapse, self.input_dim, self.output_dim)
+            (input[:, None, :, None].expand(n_data, self.n_synapse, self.input_dim, self.output_dim) -
+            self.thres[None, :,:,:].expand(n_data, self.n_synapse, self.input_dim, self.output_dim))
         x = (self.ampli[None, :, :,:]**2).expand(n_data, self.n_synapse, self.input_dim, self.output_dim) * \
-            torch.tanh(x)
-        x = x.sum(dim=(1,2)).squeeze() 
+            torch.tanh(x) 
+        x = x.sum(dim=(1)).squeeze()
+        x = x * self.scaler[None, :, :].expand(n_data, self.input_dim, self.output_dim)
+        x = x.sum(dim=(1)).squeeze() 
         return x
-        
-        
-class ParallelSynapseNeuron(nn.Module):
-    '''
-    single neuron with parallel synapse
-    '''
 
-    def __init__(self, input_dim: int, n_synapse: int, output_dim: int, input_range: Tuple = (-1.5, 1.5)) -> None:
+class ParallelSynapse2NN(nn.Module):
+    def __init__(self, 
+                input_dim: int = 28*28, 
+                hidden_dim: int = 10, 
+                n_synapse: int = 3, 
+                output_dim: int = 10, 
+                hidden_range: Tuple = (0, 40),
+                additive_bias: int = 0) -> None:
         super().__init__()
-
-        self.parallel_synapse = ParallelSynapse(
-            input_dim, n_synapse, input_range)
-        self.fc = nn.Linear(input_dim, output_dim)
-
-    def forward(self, input: Tensor):
-        '''
-        Inputs:
-            input: (n_data, input_dim)
-
-        Returns:
-            output: (n_data, output_dim)
-        '''
-        x = self.parallel_synapse(input)
-        x = self.fc(x)
-        return F.log_softmax(x, dim = 1)
-
-
-        
-class ParallelSynapseNN1(nn.Module):
-    '''
-    two layered neural network with parallel synapse at hidden layer
-    be careful the input range for parallel synapse
-    '''
-
-    def __init__(self, input_dim: int, n_synapse: int, hidden_dim: int, output_dim: int, input_range: Tuple = (-0.5, 7.5)) -> None:
-        super().__init__()
-
+        self.hidden_range = hidden_range
         self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.parallel_synapse = ParallelSynapseLayer(
-            hidden_dim, n_synapse, output_dim, input_range)
-    
-    def forward(self, input: Tensor):
-        '''
-        Inputs:
-            input: (n_data, input_dim)
-
-        Returns:
-            output: (n_data, output_dim)
-        '''
-        x = self.fc1(input) 
-        x = torch.sigmoid(x)
+        self.parallel_synapse = ParallelSynapseLayer(hidden_dim, 
+                                                n_synapse, 
+                                                output_dim, 
+                                                input_range = self.hidden_range)
+        self.additive_bias = additive_bias
+        self.dropout = nn.Dropout(0.1)
         
-        x = self.parallel_synapse(x) 
-        
-        return F.log_softmax(x, dim = 1)
-    
-class ParallelSynapseNN1Binary(ParallelSynapseNN1):
-    '''
-    trained to perform binary classification, using hinge loss
-    parallel synapses are in the hidden layer
-    '''
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        assert self.parallel_synapse.output_dim == 1
-    
-        self.theta = nn.Parameter(torch.tensor(0.0))    
-        
-    def forward(self, 
-                input: Tensor, 
-                hidden: str = 'sigmoid'):
-        x = self.fc1(input)
-        if hidden == 'sigmoid':
-            x = torch.sigmoid(x)
-        elif hidden == 'relu':
-            x = F.relu(x)
-        x = self.parallel_synapse(x)
-        return (x - self.theta).squeeze()
-    
-class ParallelSynapseNN2(nn.Module):
-    '''
-    two layered neural network with parallel synapse at input layer
-    be careful the input range for parallel synapse
-    '''
-
-    def __init__(self, input_dim: int, n_synapse: int, hidden_dim: int, output_dim: int, input_range: Tuple = (-1.5, 1.5)) -> None:
-        super().__init__()
-
-        self.parallel_synapse = ParallelSynapseLayer(
-            input_dim, n_synapse, input_range = input_range)
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, output_dim)
-        
-    def forward(self, input: Tensor):
-        '''
-        Inputs:
-            input: (n_data, input_dim)
-
-        Returns:
-            output: (n_data, output_dim)
-        '''
-        x = self.parallel_synapse(input)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.dropout(x)
         x = self.fc1(x)
-        x = torch.sigmoid(x)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim = 1)
-
-class TwoLayerNN(nn.Module):
-    '''
-    two layered neural network
-    '''
-    def __init__(self, input_dim: int, hidden_dim: int, output_dim: int) -> None:
-        super().__init__()
-
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, output_dim)
-    
-    def forward(self, input: Tensor):
-        '''
-        Inputs:
-            input: (n_data, input_dim)
-
-        Returns:
-            output: (n_data, output_dim)
-        '''
-        x = self.fc1(input)
-        x = torch.sigmoid(x)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim = 1)
-class TwoLayerNNBinary(TwoLayerNN):
-    '''
-    two layered neural network
-    '''
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs) 
-        assert self.fc2.out_features == 1
-        self.theta = nn.Parameter(torch.tensor(0.0))
         
-    def forward(self, input: Tensor, hidden = 'sigmoid'):
-        x = self.fc1(input)
-        if hidden == 'sigmoid':
-            x = torch.sigmoid(x)
-        elif hidden == 'relu':
-            x = F.relu(x) 
-        x = self.fc2(x)
+        x = torch.relu(x + self.additive_bias) 
         
-        return (x - self.theta).squeeze()
-class SingleNeuron(nn.Module):
-    '''
-    single neuron
-    '''
-
-    def __init__(self, input_dim: int, output_dim: int, input_range: Tuple = (-1, 1)) -> None:
-        super().__init__()
-
-        self.fc = nn.Linear(input_dim, output_dim)
-
-    def forward(self, input: Tensor):
-        '''
-        Inputs:
-            input: (n_data, input_dim)
-
-        Returns:
-            output: (n_data, output_dim)
-        '''
-        x = self.fc(input)
-        return F.log_softmax(x, dim = 1)
-    
+        x = self.parallel_synapse(x)
+        
+        return x
--- training.py
+++ training.py
@@ -1,7 +1,6 @@
 from network import * 
 import math
 from typing import Any
-
 import torch
 from torch import Tensor
 from torch.nn.parameter import Parameter, UninitializedParameter
@@ -14,280 +13,176 @@ import numpy as np
 import torchvision
 import torchvision.transforms as transforms
 import pickle
-import pandas as pd
-from multiprocessing import Pool
-
+import datetime
 device = torch.device("cpu")
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
 
-def hingeLoss(actv: Tensor, 
-            label: Tensor, 
-            margin = 0
-            ) -> Tensor:
+testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
+
+def multiHingeLoss(output, target, margin = 1):
     '''
     Inputs:
-        actv: (n_data, 1) 
-        label: (n_data, 1)
-        margin: scalar, default = 0
+        output: (batch_size, num_classes), direct output from final layer output
+        target: (batch_size, num_classes), must be +1/-1
+        margin: scalar, default = 1
     Outputs:
         loss: scalar
     '''
-    return (torch.maximum(torch.zeros_like(actv), margin - actv * label)).sum()
+    loss = torch.clamp(margin - output * target, min=0)
+    return loss.sum(dim=1).mean()
 
-def binary_labels(target: Tensor) -> Tensor:
-    '''
-    convert target to binary labels: 
-    (0, 1, 2, 3, 4) -> (0, 0, 0, 0, 0)
-    (5, 6, 7, 8, 9) -> (1, 1, 1, 1, 1)
-    '''
-    return (target > 4).long() * 2 - 1
-def plot_input_histogram_to_parallel_synapse_layer(model) -> None:
+def oneHotLabel(label, num_classes):
     '''
     Inputs:
-        model: nn.Module
-    
+        label: (batch_size, 1)
+        num_classes: scalar
+    Outputs:
+        one_hot_label: (batch_size, num_classes)
     '''
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-    
-    testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
-    hidden = []
-    for inputs, labels in testloader:
-        inputs = inputs.view(-1, 28*28)#.to(device)
-        
-        labels = binary_labels(labels)
-        hidden.append(torch.sigmoid(model.fc1(inputs)).detach().cpu())
-    hidden = torch.cat(hidden, dim=0)
-    plt.figure(figsize=(4, 4))
-    plt.hist(hidden.data.cpu().numpy().flatten(), bins=100)
-    plt.show(block = False)
-    
-def train_models_binary(seed,
-                        M = 3,  
-                        H = 20, 
-                        in_dim = 28, 
-                        out_dim = 1, 
-                        num_epochs = 20,
-                        verbose = True):
-    '''
-    train models for binary classification with MNIST dataset
-    
-    INPUTs:
-        seed: int,
-            manual seeds for initialization
-        M: int,
-            specifying number of parallel synapses
-        H: int,
-            specifying number of hidden units
-        num_epochs: int,
-            number of epochs for training
-            
-    OUTPUTs:
-    '''
-    assert out_dim == 1
-    torch.manual_seed(seed)
-    models = {
-        '2-NN (H={:d})'.format(H):  TwoLayerNNBinary(in_dim * in_dim, H, out_dim), \
-        '2-NN with parallel synapse (M={:d}) at hidden layer (H={:d})'.format(M, H):  ParallelSynapseNN1Binary(in_dim * in_dim, M, H, out_dim)
-    }
-    accuracies = {}
-    losses = {}
-    for m in models:
-        accuracies[m] = []
-        losses[m] = []
-        
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-    trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
+    batch_size = label.size(0)
+    one_hot_label =(-torch.ones(batch_size, num_classes)).scatter_(1, label.view(-1, 1), 1)
+    return one_hot_label
 
-    testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
-    for model_name in models:
-        model = models[model_name]
-        # Initialize model, criterion, and optimizer 
-        model.to(device)
-        if 'parallel synapse' in model_name: 
-            lr = 0.00001
-            params = [
-                {"params": model.fc1.parameters(),"weight_decay": 0.0, "learning_rate": lr},
-                {"params": model.parallel_synapse.ampli, "learning_rate": lr},
-                {"params": model.parallel_synapse.thres, "learning_rate": lr },
-                {"params": model.parallel_synapse.slope, "learning_rate": lr},
-                {"params": model.theta, "learning_rate": lr},
-            ]
-            
-        else:
-            params = model.parameters()
-            lr = 0.00005
-        optimizer = optim.Adam(params, lr=lr) #  0.00005 for parallel synapse
-        for epoch in range(num_epochs):
-            # Training loop
-            if 'parallel synapse' in model_name:
+def train_models(model, 
+                trainloader: torch.utils.data.DataLoader = trainloader, 
+                testloader: torch.utils.data.DataLoader = testloader,
+                scaler_reg: float = 0.0, 
+                out_dim: int = 10, 
+                num_epochs: int = 201,
+                verbose: bool = True, 
+                device = torch.device('cpu'),
+                model_type: str = 'parallel',
+                loss_type: str = 'nll',
+                lr: float = 0.001): 
+    assert out_dim == 10
+    
+    params = {  
+        'num_epochs': num_epochs,
+        'loss_type': loss_type,
+        'lr': lr,
+        'scaler_reg': scaler_reg
+    }
+    model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    losses, acc = [], []
+    
+    for epoch in range(num_epochs):
+        model.train() 
+        running_loss = 0.0
+        for i, (inputs, labels) in enumerate(trainloader):
+            if model_type == 'parallel':
                 with torch.no_grad():
                     slope_thres = 0.01
                     mask = (model.parallel_synapse.slope.data < slope_thres) 
                     model.parallel_synapse.slope.data = torch.clamp(model.parallel_synapse.slope.data, min = slope_thres)
-                    model.parallel_synapse.thres.data[mask] = torch.rand(mask.sum()) * 8
+                    model.parallel_synapse.thres.data[mask] = torch.rand(mask.sum()) * model.hidden_range[1] + model.hidden_range[0]
                     
-                    ampli_thres = 0.01
+                    ampli_thres = 0.1
                     mask = (model.parallel_synapse.ampli.data**2 < ampli_thres) 
-                    model.parallel_synapse.thres.data[mask] = torch.rand(mask.sum()) * 8
+                    model.parallel_synapse.thres.data[mask] = torch.rand(mask.sum()) * model.hidden_range[1] + model.hidden_range[0]
                     model.parallel_synapse.ampli.data[mask] = np.sqrt(ampli_thres)
                     
-                    model.parallel_synapse.thres.data = torch.clamp(model.parallel_synapse.thres.data, min = -0.5)
-                    # 
-                    
-            model.train() 
-            running_loss = 0.0
-            for i, (inputs, labels) in enumerate(trainloader):
-                inputs = inputs.view(-1, 28*28).to(device)
-                labels = labels.to(device)
-                optimizer.zero_grad()
-                outputs = model(inputs, hidden = 'relu')
-                # import pdb; pdb.set_trace()
-                loss = hingeLoss(outputs, binary_labels(labels), margin=1) 
-                loss.backward()
-                optimizer.step()
-                running_loss += loss.item()
-            losses[model_name].append(running_loss / len(trainloader))
-            model.eval()
-            correct = 0
-            total = 0
-            with torch.no_grad():
-                for inputs, labels in testloader:
-                    inputs = inputs.view(-1, 28*28).to(device)
-                    labels = labels.to(device)
-                    
-                    outputs = model(inputs, hidden = 'relu')
-                    # import pdb; pdb.set_trace()
-                    predicted = torch.sign(outputs).squeeze().long()
-                    total += labels.size(0)
-                    correct += predicted.eq(binary_labels(labels)).sum().item()
-                # import pdb; pdb.set_trace()
-            accuracy = 100 * correct / total 
-            accuracies[model_name].append(accuracy)
-            
-            # plt.close(1)
-            if verbose and epoch % 10 == 0:
-                # plot_input_histogram_to_parallel_synapse_layer(model)
-                print(model_name + f" Epoch {epoch+1}/{num_epochs}, Loss: {running_loss / len(trainloader):.4f}, Accuracy: {accuracy:.2f}%")
-    
-    return {'seed': seed, 
-            'M': M,
-            'models': models,
-            'accuracies': accuracies,
-            'losses': losses
-            }
-            
-def train_models(seed, 
-                M = 3,  
-                H = 20, 
-                in_dim = 28, 
-                out_dim = 10, 
-                num_epochs = 20,
-                verbose = True):
-    '''
-    INPUTs:
-        seed: int, 
-            manual seeds for initialization
-        M: int, 
-            specifying number of parallel synapses
-        save: bool = False, 
-            whether to save the model
-    OUTPUTs:
+                    # model.parallel_synapse.thres.data = torch.clamp(model.parallel_synapse.thres.data, min = model.hidden_range[0] )
         
-    '''
-    torch.manual_seed(seed)
-    models = {
-        # '1-NN':  SingleNeuron(in_dim * in_dim, out_dim), \
-        # '1-NN with parallel synapse':  ParallelSynapseNeuron(in_dim * in_dim, M, out_dim), \
-        
-        '2-NN (H={:d})'.format(H):  TwoLayerNN(in_dim * in_dim, H, out_dim), \
-        
-        '2-NN with parallel synapse (M={:d}) at hidden layer (H={:d})'.format(M, H):  ParallelSynapseNN1(in_dim * in_dim, M, H, out_dim),\
-        # '2-NN with parallel synapase (M={:d}) at input layer (H={:d})'.format(M, H):  ParallelSynapseNN2(in_dim * in_dim, M, H, out_dim)
-        }
-    accuracies = {}
-    losses = {}
-    for m in models:
-        accuracies[m] = []
-        losses[m] = []
+            inputs = inputs.view(-1, 28*28).to(device)
+            if loss_type == 'hinge':
+                labels = oneHotLabel(labels, out_dim)
+            labels = labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs) # default output: direct output from final layer
             
-    # Load the MNIST dataset
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-    trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
-
-    testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
-    
-    for model_name in models:
-        model = models[model_name]
-        # Initialize model, criterion, and optimizer 
-        model.to(device)
-        optimizer = optim.Adam(model.parameters(), lr=0.001)
-        for epoch in range(num_epochs):
-            # Training loop
-            if 'parallel synapse' in model_name:
-                with torch.no_grad():
-                    model.parallel_synapse.slope.data = torch.clamp(model.parallel_synapse.slope.data, min = 0)
-                    
-                    mask = (model.parallel_synapse.ampli.data < 0.1) 
-                    model.parallel_synapse.ampli.data = torch.clamp(model.parallel_synapse.ampli.data, min = 0.1)
-                    model.parallel_synapse.thres.data[mask] = torch.rand(mask.sum())  * 10
-                    
-                    model.parallel_synapse.thres.data = torch.clamp(model.parallel_synapse.thres.data, min = 0, max = 1)
-                
-            model.train() 
-            running_loss = 0.0
-            for i, (inputs, labels) in enumerate(trainloader):
-                inputs = inputs.view(-1, 28*28).to(device)
-                labels = labels.to(device)
-                optimizer.zero_grad()
-                outputs = model(inputs)
+            if loss_type == 'nll':
+                outputs = F.log_softmax(outputs, dim = 1)
                 loss = F.nll_loss(outputs, labels) 
-                loss.backward()
-                optimizer.step()
-                running_loss += loss.item()
-            losses[model_name].append(running_loss / len(trainloader))
-            # Validation loop for accuracy
-            model.eval()
-            correct = 0
-            total = 0
-            with torch.no_grad():
-                for inputs, labels in testloader:
-                    inputs = inputs.view(-1, 28*28).to(device)
-                    labels = labels.to(device)
-                    
-                    outputs = model(inputs)
-                    _, predicted = outputs.max(1)
-                    total += labels.size(0)
-                    correct += predicted.eq(labels).sum().item()
-
-            accuracy = 100 * correct / total 
-            accuracies[model_name].append(accuracy)
-            if verbose and epoch % 10 == 0:
-                print(model_name + f" Epoch {epoch+1}/{num_epochs}, Loss: {running_loss / len(trainloader):.4f}, Accuracy: {accuracy:.2f}%")
+            elif loss_type == 'hinge':
+                loss = multiHingeLoss(outputs, labels)
+            
+            if model_type == 'parallel':
+                loss += scaler_reg * (torch.abs(model.parallel_synapse.scaler) - 1).abs().sum()
+                
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            
+        losses.append(running_loss / len(trainloader))
+        model.eval()
+        correct, total = 0, 0
+        with torch.no_grad():
+            for inputs, labels in testloader:
+                inputs = inputs.view(-1, 28*28).to(device)
+                labels = labels.to(device)
+                outputs = model(inputs)
+                total += labels.size(0)
+                correct += torch.argmax(outputs, dim = 1).eq(labels).sum().item()
+                
+        acc.append(100 * correct / total)
+        if verbose and epoch % 10 == 0:
+            print( f"       Epoch {epoch+1}/{num_epochs}, Loss: {running_loss / len(trainloader):.4f}, Accuracy: {acc[-1]:.2f}%") 
     
-    return {'seed': seed, 
-            'M': M,
-            'models': models,
-            'accuracies': accuracies,
-            'losses': losses
-            }
-
+    
+    return {
+            'model': model,
+            'accuracy': acc,
+            'loss': losses,
+            'params': params
+    }
 
 import sys
 
 if __name__ == '__main__':
     
-    H = int(sys.argv[1])
-    M = int(sys.argv[2])
+    hidden_dim = int(sys.argv[1])
+    n_synapse = int(sys.argv[2])
     
-    experiment = []
-    for i in range(5):
-        result_dict = train_models_binary(i, H=H,M=M, num_epochs = 201)
-        experiment.append(result_dict) 
-
-        df = pd.DataFrame(experiment)
-        with open('./data/MNIST_result_H={:d}_M={:d}_.pkl'.format(H,M), 'wb') as f:
-            pickle.dump(df, f)
+    model_type = sys.argv[3]
+    loss_type = sys.argv[4]
+    
+    assert model_type in ['parallel', '2nn']
+    assert loss_type in ['nll', 'hinge']
+    
+    hidden_range = (0, 10) if hidden_dim < 40 else (0, 20)
+    bias = 10 if hidden_dim < 40 else 20
+    bias = 0 if loss_type == 'hinge' else bias
+    
+    n_Seed = 10
+    
+    '''
+    multi-seed experiment
+    '''
+    
+    results = []
+    if model_type == 'parallel':
+        file_name = f'./results/{model_type}_{loss_type}_H{hidden_dim}_M{n_synapse}_bias{bias}.pkl'  
+    else:
+        file_name = f'./results/{model_type}_{loss_type}_H{hidden_dim}_bias{bias}.pkl'
+    
+    # calculate time for training
+    start = datetime.datetime.now()
+    
+    for i in range(n_Seed):
+        print(f'Running {i+1}/{n_Seed} seed')
+        
+        end = datetime.datetime.now()
+        print(f'    Elapsed time: {end - start}')
+        
+        # define model
+        if model_type == 'parallel':
+            model = ParallelSynapse2NN(hidden_dim = hidden_dim, 
+                                    n_synapse = n_synapse,
+                                    hidden_range = hidden_range, 
+                                    additive_bias = bias)
+        elif model_type == '2nn':
+            model = TwoLayerNN(hidden_dim = hidden_dim)
+            
+        with open(f'./results/{model_type}_{loss_type}_H{hidden_dim}_M{n_synapse}_bias{bias}.pkl', 'wb') as f:
+            pickle.dump(results, f)
+        
+        
+        results.append(train_models(model, 
+                            model_type=model_type,
+                            loss_type=loss_type))
+        
+        
